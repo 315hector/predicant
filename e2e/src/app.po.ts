ff5f0ec96bce4281c48ffd746c42683e1c65257d
landa <315.hector@gmail.com>
2019-02-27 22:58:30 +0500

    add test component

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,5 +1,7 @@
 import { browser, by, element } from 'protractor';
 
+// TODO: сгенерировать анимированную гифку из пройденного теста для документации
+
 export class AppPage {
   navigateTo() {
     return browser.get('/') as Promise<any>;
@@ -9,3 +11,4 @@ export class AppPage {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { TableComponent } from './table/table.component';
+import { PnpTreeComponent } from './pnp-tree/pnp-tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'auth', component: AuthorizationComponent },
+  { path: 'address', component: AddressFormComponent },
+  { path: 'table', component: TableComponent },
+  { path: 'tree', component: PnpTreeComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
